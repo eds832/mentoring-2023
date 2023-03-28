a66a326582542f68fa025df1c6ab35120abd260d
Eduard Sardyka <eds832@gmail.com>
2023-03-28 23:08:42 +0200

    clean master

--- spring-fundation/custom-configuration/src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists "users";
-
-CREATE TABLE "users" (
-  user_id BIGINT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(20) NULL,
-  surname VARCHAR(20) NULL,
-  email VARCHAR(40) NULL,
-  PRIMARY KEY (user_id)
-);
